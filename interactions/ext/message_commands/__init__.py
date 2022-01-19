21b95ba55bf6ae8406248cf6b37c359cf95d1dd3
Toricane <73972068+Toricane@users.noreply.github.com>
2022-01-18 19:09:04 -0800

    refactor: try on bot itself

--- interactions/ext/message_commands/__init__.py
+++ interactions/ext/message_commands/__init__.py
@@ -1,5 +1,6 @@
 from . import message_commands, context, errors, prefixes
 from .message_commands import MessageCommands
+from .message_commands2 import message, logic, process
 from .context import MessageContext
 from .errors import MissingRequiredArgument
 from .prefixes import when_mentioned, when_mentioned_or
---
+++ interactions/ext/message_commands/_setup.py
@@ -0,0 +1,19 @@
+from interactions import Client
+from .message_commands2 import message, logic, process
+from types import MethodType
+from typing import Sequence, Union
+
+
+def setup(bot: Client, prefix: Union[Sequence[str], str]) -> None:
+    """
+    Apply hooks to a bot to add additional features
+    This function is required, as importing alone won't extend the classes
+    """
+    bot.message = MethodType(message, bot)
+    bot.logic = MethodType(logic, bot)
+    bot.process = MethodType(process, bot)
+
+    bot.prefix = prefix
+    bot.__commands__ = {}
+
+    bot.event(process, "on_message_create")
