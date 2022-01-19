4b355195ef6f8534eaa888ddf923e6a1ca3db94f
Toricane <73972068+Toricane@users.noreply.github.com>
2022-01-18 18:19:04 -0800

    feat: use built-in Channel.send

--- interactions/ext/message_commands/context.py
+++ interactions/ext/message_commands/context.py
@@ -1,5 +1,5 @@
-from interactions import ActionRow, Button, Context, Message, SelectMenu
-from typing import List, Optional
+from interactions import Channel, Context
+from typing import Optional
 
 
 class MessageContext(Context):
@@ -8,6 +8,8 @@ class MessageContext(Context):
     def __init__(self, _client, **kwargs):
         super().__init__(**kwargs)
         self._client = _client
+        self.channel._json["_client"] = _client
+        self.channel = Channel(**self.channel._json)
 
     async def send(
         self,
@@ -39,135 +41,10 @@ class MessageContext(Context):
         await ctx.send(content="Hello world!")
         ```
         """
-        _content: str = "" if content is None else content
-        _tts: bool = False if tts is None else tts
-        # _file = None if file is None else file
-        # _attachments = [] if attachments else None
-        _embeds: list = []
-        _allowed_mentions: dict = {} if allowed_mentions is None else allowed_mentions
-        _components: List[dict] = [{"type": 1, "components": []}]
-        if embeds:
-            if isinstance(embeds, list):
-                _embeds = [embed._json for embed in embeds]
-            else:
-                _embeds = [embeds._json]
-
-        # TODO: Break this obfuscation pattern down to a "builder" method.
-        if components:
-            if isinstance(components, list) and all(
-                isinstance(action_row, ActionRow) for action_row in components
-            ):
-                _components = [
-                    {
-                        "type": 1,
-                        "components": [
-                            (
-                                component._json
-                                if component._json.get("custom_id")
-                                or component._json.get("url")
-                                else []
-                            )
-                            for component in action_row.components
-                        ],
-                    }
-                    for action_row in components
-                ]
-            elif isinstance(components, list) and all(
-                isinstance(component, (Button, SelectMenu)) for component in components
-            ):
-                for component in components:
-                    if isinstance(component, SelectMenu):
-                        component._json["options"] = [
-                            options._json if not isinstance(options, dict) else options
-                            for options in component._json["options"]
-                        ]
-                _components = [
-                    {
-                        "type": 1,
-                        "components": [
-                            (
-                                component._json
-                                if component._json.get("custom_id")
-                                or component._json.get("url")
-                                else []
-                            )
-                            for component in components
-                        ],
-                    }
-                ]
-            elif isinstance(components, list) and all(
-                isinstance(action_row, (list, ActionRow)) for action_row in components
-            ):
-                _components = []
-                for action_row in components:
-                    for component in (
-                        action_row
-                        if isinstance(action_row, list)
-                        else action_row.components
-                    ):
-                        if isinstance(component, SelectMenu):
-                            component._json["options"] = [
-                                option._json for option in component.options
-                            ]
-                    _components.append(
-                        {
-                            "type": 1,
-                            "components": [
-                                (
-                                    component._json
-                                    if component._json.get("custom_id")
-                                    or component._json.get("url")
-                                    else []
-                                )
-                                for component in (
-                                    action_row
-                                    if isinstance(action_row, list)
-                                    else action_row.components
-                                )
-                            ],
-                        }
-                    )
-            elif isinstance(components, ActionRow):
-                _components[0]["components"] = [
-                    (
-                        component._json
-                        if component._json.get("custom_id")
-                        or component._json.get("url")
-                        else []
-                    )
-                    for component in components.components
-                ]
-            elif isinstance(components, Button):
-                _components[0]["components"] = (
-                    [components._json]
-                    if components._json.get("custom_id") or components._json.get("url")
-                    else []
-                )
-            elif isinstance(components, SelectMenu):
-                components._json["options"] = [
-                    options._json if not isinstance(options, dict) else options
-                    for options in components._json["options"]
-                ]
-                _components[0]["components"] = (
-                    [components._json]
-                    if components._json.get("custom_id") or components._json.get("url")
-                    else []
-                )
-        else:
-            _components = []
-
-        # TODO: post-v4: Add attachments into Message obj.
-        payload = Message(
-            content=_content,
-            tts=_tts,
-            # file=file,
-            # attachments=_attachments,
-            embeds=_embeds,
-            allowed_mentions=_allowed_mentions,
-            components=_components,
+        return await self.channel.send(
+            content,
+            tts=tts,
+            embeds=embeds,
+            allowed_mentions=allowed_mentions,
+            components=components,
         )
-
-        res = await self._client.create_message(
-            channel_id=int(self.channel.id), payload=payload._json
-        )
-        return Message(**res, _client=self._client)
