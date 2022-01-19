d10d12b5c9d3541bdfc492c79b406f2abe6e1ae8
Toricane <73972068+Toricane@users.noreply.github.com>
2022-01-18 19:14:25 -0800

    fix: importerror

--- interactions/ext/message_commands/__init__.py
+++ interactions/ext/message_commands/__init__.py
@@ -1,4 +1,6 @@
 from . import message_commands, context, errors, prefixes
+from . import _setup as setup
+from ._setup import setup
 from .message_commands import MessageCommands
 from .message_commands2 import message, logic, process
 from .context import MessageContext
