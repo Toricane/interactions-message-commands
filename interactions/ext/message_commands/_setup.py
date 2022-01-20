b661518bce73c5a1ae183c9275bf4f09d4222428
Toricane <73972068+Toricane@users.noreply.github.com>
2022-01-19 19:02:21 -0800

    feat: add bot.when_mentioned[_or]

--- interactions/ext/message_commands/_setup.py
+++ interactions/ext/message_commands/_setup.py
@@ -1,10 +1,13 @@
 from interactions import Client
 from .message_commands3 import MessageCommands, message
+from .prefixes import when_mentioned, when_mentioned_or
 from types import MethodType
 
 
 def setup(bot: Client) -> None:
     MessageCommands(bot)
     bot.message = MethodType(message, bot)
+    bot.when_mentioned = MethodType(when_mentioned, bot)
+    bot.when_mentioned_or = MethodType(when_mentioned_or, bot)
 
     bot.event(bot.process, "on_message_create")
