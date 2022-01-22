61355be3428c28d41f4d6bb32a469ddc03a130c6
Toricane <73972068+Toricane@users.noreply.github.com>
2022-01-21 17:14:33 -0800

    chore: never mind

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="interactions-message-commands",
-    version="0.1.0.0",
+    version="0.1.0",
     description="Message commands extension for discord-py-interactions",
     long_description=long_description,
     long_description_content_type="text/markdown",
