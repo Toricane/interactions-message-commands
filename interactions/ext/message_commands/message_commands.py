f456588ccedff695fbe0da2dc99f5e3962434ab5
Toricane <73972068+Toricane@users.noreply.github.com>
2022-01-21 16:40:42 -0800

    refactor: improve code quality

--- interactions/ext/message_commands/message_commands.py
+++ interactions/ext/message_commands/message_commands.py
@@ -1,7 +1,7 @@
 from inspect import _empty, iscoroutinefunction
-from interactions import Client, Extension
+from interactions import Client, Extension, Message
 from shlex import split
-from typing import List, Optional, Set, Tuple, Union, Sequence, Coroutine
+from typing import List, Optional, Union, Sequence, Coroutine
 
 from .errors import (
     MissingRequiredArgument,
@@ -29,31 +29,7 @@ class MessageCommands(Extension):
 
         # if the prefix is a coroutine, run it
         if iscoroutinefunction(self.bot.prefix):
-            message = msg._json
-            member = msg.member._json
-            user = msg.author._json
-            # get from cache if possible
-            channel = self.bot.http.cache.channels.get(
-                str(msg.channel_id)
-            ) or await self.bot.http.get_channel(msg.channel_id)
-            channel = channel if isinstance(channel, dict) else channel._json
-            guild = self.bot.http.cache.guilds.get(
-                str(msg.guild_id)
-            ) or await self.bot.http.get_guild(msg.guild_id)
-            guild = guild if isinstance(guild, dict) else guild._json
-
-            for i in [message, member, user, channel, guild]:
-                i.pop("_client", None)
-
-            ctx = MessageContext(
-                self.bot.http,
-                message=message,
-                member=member,
-                user=user,
-                channel=channel,
-                guild=guild,
-            )
-
+            ctx = self.create_context(msg)
             prefix = await self.bot.prefix(ctx)
 
             if msg.content.startswith(prefix):
@@ -93,34 +69,7 @@ class MessageCommands(Extension):
             return
 
         # get required data for MessageContext
-        if not context:
-            message = msg._json
-            member = msg.member._json
-            user = msg.author._json
-            # get from cache if possible
-            channel = self.bot.http.cache.channels.get(
-                str(msg.channel_id)
-            ) or await self.bot.http.get_channel(msg.channel_id)
-            channel = channel if isinstance(channel, dict) else channel._json
-            guild = self.bot.http.cache.guilds.get(
-                str(msg.guild_id)
-            ) or await self.bot.http.get_guild(msg.guild_id)
-            guild = guild if isinstance(guild, dict) else guild._json
-
-            for i in [message, member, user, channel, guild]:
-                i.pop("_client", None)
-
-            ctx = MessageContext(
-                self.bot.http,
-                message=message,
-                member=member,
-                user=user,
-                channel=channel,
-                guild=guild,
-            )
-        else:
-            ctx = context
-
+        ctx = self.create_context(msg) if not context else context
         func: callable = self.bot.message_commands[
             content[0]
         ]  # get the corresponding function
@@ -178,3 +127,30 @@ class MessageCommands(Extension):
             return await func(func.__self__, ctx, *(ctx.args + ctx._args), **ctx.kwargs)
         else:
             return await func(ctx, *(ctx.args + ctx._args), **ctx.kwargs)
+
+    async def create_context(self, msg: Message) -> MessageContext:
+        """Returns a MessageContext object"""
+        message = msg._json
+        member = msg.member._json
+        user = msg.author._json
+        # get from cache if possible
+        channel = self.bot.http.cache.channels.get(
+            str(msg.channel_id)
+        ) or await self.bot.http.get_channel(msg.channel_id)
+        channel = channel if isinstance(channel, dict) else channel._json
+        guild = self.bot.http.cache.guilds.get(
+            str(msg.guild_id)
+        ) or await self.bot.http.get_guild(msg.guild_id)
+        guild = guild if isinstance(guild, dict) else guild._json
+
+        for i in (message, member, user, channel, guild):
+            i.pop("_client", None)
+
+        return MessageContext(
+            self.bot.http,
+            message=message,
+            member=member,
+            user=user,
+            channel=channel,
+            guild=guild,
+        )
