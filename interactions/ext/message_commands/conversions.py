161ba5a81a205b44aaa0a457f617186457a1c29e
Toricane <73972068+Toricane@users.noreply.github.com>
2022-01-22 13:03:12 -0800

    refactor: debug

--- interactions/ext/message_commands/conversions.py
+++ interactions/ext/message_commands/conversions.py
@@ -84,10 +84,10 @@ def resolve_union(self, _type=None):
     if not args:
         return False
     self.input = split(self.input) if len(split(self.input)) > 1 else list(self.input)
-    print("input", input)
+    print("input", self.input)
     print("args", args)
     for arg in args:
-        if get_origin(arg) is List and resolve_list(self, arg):
+        if arg is List and resolve_list(self, arg):
             print("yes")
             return True
         resolved = resolve_basic_typehint(self, arg)
