c39dc4e8a6d756f964bdf4192885130435d26ff0
Toricane <73972068+Toricane@users.noreply.github.com>
2022-01-22 11:47:34 -0800

    fix: list and union

--- interactions/ext/message_commands/conversions.py
+++ interactions/ext/message_commands/conversions.py
@@ -1,5 +1,6 @@
 from typing import Union, List, get_origin, get_args
 from inspect import _empty
+from shlex import split
 
 
 def not_needed(self):
@@ -8,22 +9,39 @@ def not_needed(self):
 
 
 def to_int(self):
-    try:
-        self.input = int(self.input)
-    except ValueError:
+    if isinstance(self.input, list):
+        for i in self.input:
+            try:
+                self.input[self.input.index(i)] = int(i)
+            except ValueError:
+                try:
+                    self.input[self.input.index(i)] = int(float(self.input))
+                except ValueError:
+                    return False
+    else:
         try:
-            self.input = int(float(self.input))
+            self.input = int(self.input)
         except ValueError:
-            return False
-    return True
+            try:
+                self.input = int(float(self.input))
+            except ValueError:
+                return False
+        return True
 
 
 def to_float(self):
-    try:
-        self.input = float(self.input)
-    except ValueError:
-        return False
-    return True
+    if isinstance(self.input, list):
+        for i in self.input:
+            try:
+                self.input[self.input.index(i)] = float(i)
+            except ValueError:
+                return False
+    else:
+        try:
+            self.input = float(self.input)
+        except ValueError:
+            return False
+        return True
 
 
 def resolve_basic_typehint(self, _type=None) -> bool:
@@ -63,6 +81,7 @@ def resolve_union(self, _type=None):
     args = get_args(self.type) if _type is None else get_args(_type)
     if not args:
         return False
+    self.input = split(self.input) if len(split(self.input)) > 1 else list(self.input)
     for arg in args:
         if get_origin(arg) is List and resolve_list(self, arg):
             return True
