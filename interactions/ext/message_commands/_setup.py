a1c5d449d4345b3c82060dce755ee78e55db48d1
Toricane <73972068+Toricane@users.noreply.github.com>
2022-01-23 18:27:50 -0800

    refactor: works

--- interactions/ext/message_commands/_setup.py
+++ interactions/ext/message_commands/_setup.py
@@ -5,8 +5,7 @@ from .prefixes import when_mentioned, when_mentioned_or
 from types import MethodType
 
 
-def setup(bot: Client):  # $, *args, **kwargs) -> None:
-    # print(f"{args}, {kwargs}")
+def setup(bot: Client, *args, **kwargs) -> None:
     MessageCommands(bot)
     bot.message = MethodType(message, bot)
     bot.when_mentioned = MethodType(when_mentioned, bot)
